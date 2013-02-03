5e685f3739dc2858e630b27ac1ba5045fefa2d11
falk <falk@logicparty.org>
2013-02-03 00:26:37 -0500

    add buy link for sweater

--- routes/index.js
+++ routes/index.js
@@ -41,5 +41,13 @@ exports.splash = function(req, res){
 exports.buy = function(req, res){
   var url = req.param('url');
 
-  res.render('buy.handlebars', { title: 'Mannequin', url: url });
+  var buy = false
+  var buy_link = ""
+
+  if (url == 'http://24.media.tumblr.com/78c49cba82cdcba571c54f82ef5eb69b/tumblr_mhmiujnkqM1s53qw2o1_1280.jpg') {
+    buy = true
+    buy_link = "http://www.hotsquash.com/products/womenstop?mc=1&sc=1"
+  }
+
+  res.render('buy.handlebars', { title: 'Mannequin', url: url, buy: buy, buy_link: buy_link });
 };
