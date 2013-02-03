dd8fc1c6540fab22d696518fbce64c0da6194df2
falk <falk@logicparty.org>
2013-02-03 10:44:53 -0500

    remove unused routes

--- app.js
+++ app.js
@@ -5,7 +5,6 @@
 
 var express = require('express')
   , routes = require('./routes')
-  , user = require('./routes/user')
   , http = require('http')
   , handlebars = require('express3-handlebars')
   , stylus = require('stylus')
@@ -35,7 +34,6 @@ app.configure('development', function(){
 app.get('/', routes.index);
 app.get('/splash', routes.splash);
 app.get('/buy', routes.buy);
-app.get('/users', user.list);
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
